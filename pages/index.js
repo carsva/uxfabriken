bc88eb2fd8413790665602655b1b454c0b8789e2
Carl Svahnstrom <carl@tocco.se>
2018-11-23 15:02:00 +0100

    Added new logo and custom background color

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import styled from "styled-components";
+
+const BodyStyle = styled.body`
+  background: #00183C;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoStyle = styled.img`
+  width: 80%;
+  margin-top: 0em;
+`;
 
 class Index extends Component {
   render() {
     return (
+      <BodyStyle>
       <div>
-         <img src={`/static/img/uxfabriken_logo.svg`} />
+         <LogoStyle src={`/static/img/uxfabriken_logo.svg`} />
       </div>
+      </BodyStyle>
 
     );
   }
