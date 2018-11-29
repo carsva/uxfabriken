3411c234aa58d8c6a3d751436b1fa222529410bc
Carl Svahnstrom <carl@tocco.se>
2018-11-29 10:33:18 +0100

    now background changes after reload

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import styled from "styled-components";
-import posed from 'react-pose';
 import './../normalize.css';
 import './../index.css';
 
 const BodyStyle = styled.div`
-  background: palevioletred;
   display: flex;
   align-content: center;
   justify-content: center;
 `;
 
-const Box = posed.div({
-  visible: { opacity: 1 },
-  hidden: { opacity: 0 }
-});
-
 const LogoStyle = styled.img`
-  width: 60%;
+  width: 50%;
   margin-top: 16em;
 `;
 
 class Index extends Component {
+
+  componentDidMount() {
+    function random_bg_color() {
+      var x = Math.floor(Math.random() * 256);
+      var y = Math.floor(Math.random() * 256);
+      var z = Math.floor(Math.random() * 256);
+      var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+   console.log(bgColor);
+    
+      document.body.style.background = bgColor;
+      }
+  
+  random_bg_color();
+  };
+
   render() {
+    
     return (
       <BodyStyle>
          <LogoStyle src={`/static/img/uxfabriken_logo.svg`} />
